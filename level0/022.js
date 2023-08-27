59fd4ccd245fe392910870ab5d69ace96cd530ff
KoJaeHyeong(고재형) <kojae2423@mogoscorp.com>
2023-08-27 21:46:29 +0900

    :sparkles: 코드 처리하기

--- level0/022.js
+++ level0/022.js
@@ -64,3 +64,18 @@ function solution(code) {
   }
   return (answer = answer.length !== 0 ? answer : "EMPTY");
 }
+
+function solution(code) {
+  let answer = "";
+  let mode = 0;
+
+  for (let i = 0; i < code.length; i += 1) {
+    if (Number(code[i]) === 1) {
+      mode = mode === 1 ? 0 : 1;
+    }
+    if (Number(code[i]) !== 1 && i % 2 === mode) {
+      answer += code[i];
+    }
+  }
+  return answer.length > 0 ? answer : "EMPTY";
+}
